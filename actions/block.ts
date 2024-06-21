ac049682a0d0fc7bed603aa8440fadd76c2a5fb3
XinranRen <karasuba0613@outlook.com>
2024-06-20 22:42:30 -0400

    Update

---
+++ actions/block.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { getSelf } from "@/lib/auth-service";
+import { blockUser, unblockUser } from "@/lib/block-service";
+import { revalidatePath } from "next/cache";
+
+export const onBlock = async (id: string) => {
+  const self = await getSelf();
+
+  const blockedUser = await blockUser(id);
+
+  revalidatePath("/");
+
+  if (blockedUser) {
+    revalidatePath(`/${blockedUser.blocked.username}`);
+  }
+
+  return blockedUser;
+};
+
+export const onUnblock = async (id: string) => {
+  const self = await getSelf();
+
+  const unblockedUser = await unblockUser(id);
+
+  revalidatePath("/");
+
+  if (unblockedUser) {
+    revalidatePath(`/${unblockedUser.blocked.username}`);
+  }
+
+  return unblockedUser;
+};
